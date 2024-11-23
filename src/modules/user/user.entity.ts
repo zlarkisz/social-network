b5c2fbcbc30f630cbbdbeeee6f407fb55e3c5f58
Gleb Bufin <gleb.bufin@paybis.com>
2024-11-23 19:09:27 +0200

    Add User entity for database

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ length: 50 })
+  email: string;
+
+  @Column({ length: 50 })
+  password: string;
+}
