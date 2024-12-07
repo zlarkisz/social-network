2399764a5d2b89dfae571c0a57ce7e1eb392171d
Gleb Bufin <gleb.bufin@paybis.com>
2024-12-07 11:43:57 +0200

    feat(): Configure Swagger to document APIs

--- src/modules/posts/dto/create-post.dto.ts
+++ src/modules/posts/dto/create-post.dto.ts
@@ -1,14 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { IsString, Length, IsNumber, IsNotEmpty } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({
+    description: 'The title of the post',
+    example: 'My First Post',
+    minLength: 3,
+    maxLength: 30,
+  })
   @IsString()
   @Length(3, 30)
   title: string;
 
+  @ApiProperty({
+    description: 'The content of the post',
+    example: 'This is the content of my first post.',
+    minLength: 6,
+    maxLength: 500,
+  })
   @IsString()
   @Length(6, 500)
   content: string;
 
+  @ApiProperty({
+    description: 'The ID of the user creating the post',
+    example: 1,
+  })
   @IsNumber()
   @IsNotEmpty()
   userId: number;
--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Get, Body, Param, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { CreatePostDto } from './dto/create-post.dto';
 import { Post as PostEntity } from './post.entity';
@@ -6,22 +7,42 @@ import { PostsService } from './posts.service';
 import { ForbiddenGuard } from '../../guards/forbidden.guard';
 import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
 
+@ApiTags('Posts')
 @UseGuards(JwtAuthGuard)
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @ApiOperation({ summary: 'Create a new post' })
+  @ApiResponse({
+    status: 201,
+    description: 'Post successfully created.',
+    type: PostEntity,
+  })
+  @ApiResponse({ status: 400, description: 'Validation error.' })
   @UseGuards(ForbiddenGuard)
   @Post()
   create(@Body() createPostDto: CreatePostDto): Promise<PostEntity> {
     return this.postsService.create(createPostDto);
   }
 
+  @ApiOperation({ summary: 'Retrieve all posts' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of posts.',
+    type: [PostEntity],
+  })
   @Get()
   findAll(): Promise<PostEntity[]> {
     return this.postsService.findAll();
   }
 
+  @ApiOperation({ summary: 'Retrieve posts by user ID' })
+  @ApiResponse({ status: 200, description: 'Posts found.', type: [PostEntity] })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found or no posts available.',
+  })
   @Get('/user/:userId')
   async findPostsByUser(
     @Param('userId') userId: number,
